58c4f03f448a7b96b1e50eefb83b9733578d8441
Brooks West <brooksmwest@hotmail.com>
2025-01-05 17:24:48 -0600

    got the endpoint from client credentials

---
+++ src/api/greetings.js
@@ -0,0 +1,18 @@
+import { clientCredentials } from '@/utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getGreetings = () =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+export default getGreetings;
